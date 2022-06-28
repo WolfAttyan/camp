6dc360654510ea87d6922c20a62f1523a4bd8419
WolfAttyan <attyandayo0802@gmail.com>
2022-06-28 15:07:53 +0900

    6/28のあっちゃん

--- camp/ViewController.swift
+++ camp/ViewController.swift
@@ -6,37 +6,82 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
-
+    
+    // アプリが起動した時
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTemper(keyword: "searchWord")
     }
     
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var weather: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var windspeed: UILabel!
+    @IBOutlet weak var precipitation: UILabel!
     
     func searchBarSearchButtonClicked(_ srarchBar: UISearchBar) {
         
         view.endEditing(true)
         
-        if let searchWord = searchBar.text {
+        if let searchWord = searchText.text{
             print(searchWord)
             searchTemper(keyword: searchWord)
         }
     }
-
+    
     func searchTemper(keyword: String){
-        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            return
-    }
-        guard let req_url = URL(string:
-        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-        ) else {
-            return
+        let url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation,weathercode,windspeed_10m"
+        AF
+            .request(url) // URLにリクエストを送る(情報ちょうだい)
+            .responseDecodable(of: JSONResult.self) { response in // JSONResult型に返ってきた注文を変換する
+            switch response.result {
+            case .success(let data): // 成功した時
+                dump(data)
+                print(data.hourly.temperature_2m[0])
+            case .failure(let error): // 失敗した時
+                print("error")
+            }
         }
-        print(req_url)
     }
-    }
+    
+}
 
+
+//        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+//            return
+//        }
+//        guard let req_url = URL(string:
+//                                    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation,weathercode,windspeed_10m"
+//        ) else {
+//            return
+//        }
+//        print(req_url)
+
+struct ItemJson: Codable{
+    let name: String?
+    let maker: String?
+    let url: URL?
+    let image: URL?
+}
+
+struct RestltJson: Codable {
+    let item:[ItemJson]?
+}
+
+
+struct JSONResult: Codable {
+    let hourly: Temperature
+}
+
+struct Temperature: Codable {
+    let temperature_2m: [Double]
+    let weathercode: [Int]
+    let windspeed_10m: [Double]
+    let precipitation: [Double]
+}
