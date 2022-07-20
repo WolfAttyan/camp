07bbd2608e34f817da20b5b3a9aac620caa05268
WolfAttyan <attyandayo0802@gmail.com>
2022-07-20 15:39:49 +0900

    0720

---
+++ Pods/Target Support Files/Pods-camp-campUITests/Pods-camp-campUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_camp_campUITests : NSObject
+@end
+@implementation PodsDummy_Pods_camp_campUITests
+@end
---
+++ Pods/Target Support Files/Pods-camp/Pods-camp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_camp : NSObject
+@end
+@implementation PodsDummy_Pods_camp
+@end
